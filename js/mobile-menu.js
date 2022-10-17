e878011b617ef7037f25c822aee13baa4d45cff5
khavrat <ihavrat@gmail.com>
2022-10-17 08:40:33 +0300

    final version

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,15 +2,17 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const bodyScrollLock = document.querySelector('body');
 
-  function toggleMenu() {
+  const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-   bodyScrollLock.classList.toggle('no-scroll')
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
